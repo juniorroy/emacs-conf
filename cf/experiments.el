427b659645f2e1b5ac9ef954a86e768a13ea7da3
juniorroy <juniorroy.web@gmail.com>
2014-01-19 00:05:08 +0700

    Write routine and bind it to move cursor to previous window

--- cf/experiments.el
+++ cf/experiments.el
@@ -1,2 +1,11 @@
 ;;redefining key bind to invoke other-window command
 (global-set-key "\C-x\C-n" 'other-window)
+
+;;define function to move cursor to previous window
+;;because emacs haven't predefined function or command
+(defun other-window-backward ()
+  "Select previous window."
+  (interactive)
+  (other-window -1))
+;;;and bind it to key
+(global-set-key "\C-x\C-p" 'other-window-backward)
